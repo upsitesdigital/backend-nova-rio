68b9a53808d150162e13d616d10cb8584df03a1a
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-11 14:49:30 -0300

    feat: add swagger ApiProperty decorators to auth DTOs

--- src/auth/dto/admin-login.dto.ts
+++ src/auth/dto/admin-login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class AdminLoginDto {
+  @ApiProperty({ example: 'admin@novario.com' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'Admin@2026!' })
   @IsString()
   @MinLength(6)
   password: string;
