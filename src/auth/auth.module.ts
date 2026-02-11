4ff39d70ebbd347a81450e717f1f57483553efdf
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-11 13:19:49 -0300

    feat: add auth controller and module wiring

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AdminLoginUseCase } from './application/use-cases/admin-login.use-case.js';
+import { ClientLoginUseCase } from './application/use-cases/client-login.use-case.js';
+import { ClientRegisterUseCase } from './application/use-cases/client-register.use-case.js';
+import { ForgotPasswordUseCase } from './application/use-cases/forgot-password.use-case.js';
+import { GetProfileUseCase } from './application/use-cases/get-profile.use-case.js';
+import { RefreshTokenUseCase } from './application/use-cases/refresh-token.use-case.js';
+import { AuthController } from './auth.controller.js';
+import { ADMIN_REPOSITORY } from './domain/interfaces/admin.repository.interface.js';
+import { CLIENT_REPOSITORY } from './domain/interfaces/client.repository.interface.js';
+import { HASH_SERVICE } from './domain/interfaces/hash.service.interface.js';
+import { TOKEN_SERVICE } from './domain/interfaces/token.service.interface.js';
+import { PrismaAdminRepository } from './infrastructure/repositories/prisma-admin.repository.js';
+import { PrismaClientRepository } from './infrastructure/repositories/prisma-client.repository.js';
+import { BcryptHashService } from './infrastructure/services/bcrypt-hash.service.js';
+import { JwtTokenService } from './infrastructure/services/jwt-token.service.js';
+import { JwtStrategy } from './strategies/jwt.strategy.js';
+
+@Module({
+  imports: [PassportModule, JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [
+    JwtStrategy,
+    { provide: HASH_SERVICE, useClass: BcryptHashService },
+    { provide: TOKEN_SERVICE, useClass: JwtTokenService },
+    { provide: CLIENT_REPOSITORY, useClass: PrismaClientRepository },
+    { provide: ADMIN_REPOSITORY, useClass: PrismaAdminRepository },
+    ClientRegisterUseCase,
+    ClientLoginUseCase,
+    AdminLoginUseCase,
+    RefreshTokenUseCase,
+    ForgotPasswordUseCase,
+    GetProfileUseCase,
+  ],
+})
+export class AuthModule {}
