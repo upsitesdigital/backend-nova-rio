948a10782d79818b28b4b59ad4c72bb14237865a
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-11 13:19:43 -0300

    feat: add auth DTOs, guards, decorators and JWT strategy

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { ROLES_KEY } from '../decorators/roles.decorator.js';
+
+interface AuthUser {
+  id: string;
+  email: string;
+  type: string;
+  role?: string;
+}
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const user = request.user as AuthUser;
+    return requiredRoles.includes(user.role ?? '');
+  }
+}
