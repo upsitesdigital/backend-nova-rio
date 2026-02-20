3a7d1e1ba016373e0785992cc14a070df0ac1ad0
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-19 21:29:39 -0300

    feat: register holidays and schedule modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AdminUsersModule } from './admin-users/admin-users.module.js';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { AuthModule } from './auth/auth.module.js';
 import { ClientsModule } from './clients/clients.module.js';
 import { EmployeesModule } from './employees/employees.module.js';
+import { HolidaysModule } from './holidays/holidays.module.js';
 import { PackagesModule } from './packages/packages.module.js';
 import { ServicesModule } from './services/services.module.js';
 import { PrismaModule } from './shared/prisma/prisma.module.js';
@@ -14,12 +16,14 @@ import { UnitsModule } from './units/units.module.js';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
     PrismaModule,
     AuthModule,
     ServicesModule,
     AdminUsersModule,
     ClientsModule,
     EmployeesModule,
+    HolidaysModule,
     PackagesModule,
     UnitsModule,
   ],
