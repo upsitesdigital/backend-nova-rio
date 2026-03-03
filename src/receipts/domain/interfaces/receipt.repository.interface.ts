48bd803d76bc04d8815e2c98dc8e3780115cca1f
Barba2k2 <lorenzo.dz@hotmail.com>
2026-03-03 16:23:14 -0300

    feat: add receipt domain interfaces

---
+++ src/receipts/domain/interfaces/receipt.repository.interface.ts
@@ -0,0 +1,19 @@
+export const RECEIPT_REPOSITORY = Symbol('RECEIPT_REPOSITORY');
+
+export interface ReceiptResponse {
+  id: number;
+  uuid: string;
+  fileUrl: string;
+  paymentId: number;
+  createdAt: Date;
+}
+
+export interface CreateReceiptData {
+  paymentId: number;
+  fileUrl: string;
+}
+
+export interface IReceiptRepository {
+  createReceipt(data: CreateReceiptData): Promise<ReceiptResponse>;
+  findReceiptByPaymentId(paymentId: number): Promise<ReceiptResponse | null>;
+}
