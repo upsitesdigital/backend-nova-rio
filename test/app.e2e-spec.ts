06a02b920bcbc284d033168a20c9aeb561cbc064
Pedrinho Hilario <flygames1214@gmail.com>
2026-03-11 00:00:34 -0300

    test: testando pipeline de CI/CD

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,7 +15,6 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-
   it('/ (GET)', () => {
     return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
