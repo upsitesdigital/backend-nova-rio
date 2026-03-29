e5c69d34fc9f09ec950befe595ee1f7917c231fb
Pedrinho Hilario <flygames1214@gmail.com>
2026-03-29 15:55:33 -0300

    fix(prisma): usar tsconfig.seed no comando de migration seed

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
-    seed: 'ts-node --compiler-options {"module":"commonjs"} prisma/seed.ts',
+    seed: 'ts-node --project tsconfig.seed.json prisma/seed.ts',
   },
   datasource: {
     url: env('DATABASE_URL'),
